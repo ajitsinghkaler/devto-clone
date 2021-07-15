20e44dd9a0b045064d738ff9ad51d315db25ec64
Ajit Singh <ajitsinghkaler0@gmail.com>
2021-07-15 12:04:55 +0530

    readme changed

--- src/app/pages/home/sidebar-social-links/sidebar-social-links.component.ts
+++ src/app/pages/home/sidebar-social-links/sidebar-social-links.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-sidebar-social-links',
   templateUrl: './sidebar-social-links.component.html',
-  styleUrls: ['./sidebar-social-links.component.scss']
+  styleUrls: ['./sidebar-social-links.component.scss'],
 })
 export class SidebarSocialLinksComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
