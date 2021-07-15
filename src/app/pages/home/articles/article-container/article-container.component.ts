9fcf4f65429158929176443d9da715b19ad47282
Ajit Singh <ajitsinghkaler0@gmail.com>
2021-07-15 12:44:17 +0530

    fixed styling and folder structure

---
+++ src/app/pages/home/articles/article-container/article-container.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-article-container',
+  templateUrl: './article-container.component.html',
+  styleUrls: ['./article-container.component.scss']
+})
+export class ArticleContainerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/pages/home/articles/article-header/article-header.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-article-header',
+  templateUrl: './article-header.component.html',
+  styleUrls: ['./article-header.component.scss']
+})
+export class ArticleHeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
