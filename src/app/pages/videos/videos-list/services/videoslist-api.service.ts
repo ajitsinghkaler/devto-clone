5bc741f127b3f72449b2e9ff20852aea2be404ae
Ajit Singh <ajitsinghkaler0@gmail.com>
2021-08-20 12:05:46 +0530

    remove one more unsed component

--- src/app/pages/videos/videos-list/services/videoslist-api.service.ts
+++ src/app/pages/videos/videos-list/services/videoslist-api.service.ts
@@ -5,28 +5,15 @@ import { VideosList } from '../../../../models/videosList';
 import { environment } from 'src/environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class VideoslistApiService {
+  constructor(private http: HttpClient) {}
 
-    constructor(private http: HttpClient) {}
-  
-    getVideoslist(
-      params?: Record<string, string>
-    ): Observable<VideosList[]> {
-      const newParams = new HttpParams({ fromObject: params }).toString();
-      return this.http.get<VideosList[]>(
-        `${environment.baseApi}/videos?${newParams}`
-      );
-    }
-  
-    // createObjectToParams(params?: Record<string, string>) {
-    //   let requestParams = new HttpParams();
-    //   if(params){
-    //      requestParams = requestParams.set('page', params.page);
-    //      requestParams = requestParams.set('signature', params.signature);
-    //   }
-    //   return requestParams.toString();
-    // }
+  getVideoslist(params?: Record<string, string>): Observable<VideosList[]> {
+    const newParams = new HttpParams({ fromObject: params }).toString();
+    return this.http.get<VideosList[]>(
+      `${environment.baseApi}/videos?${newParams}`
+    );
   }
-  
+}
--- src/app/pages/videos/videos.component.ts
+++ src/app/pages/videos/videos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { VideosListStore } from './videos-list/services/videos-list.store';
 
 @Component({
   selector: 'app-videos',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VideosComponent implements OnInit {
 
-  constructor() { }
+  page: string = '0';
+  videosList$ = this.VideosListStore.VideosList$;
+
+  constructor(private VideosListStore: VideosListStore) {}
 
   ngOnInit(): void {
+    this.VideosListStore.getVideoslist({ page: this.page, signature:'4072170' });
+    console.log(this.videosList$)
   }
-
+  
+   onScrollingFinished(){
+    this.page = (Number(this.page)+1).toString()
+    this.VideosListStore.getVideoslist({ page: this.page, signature:'4072170' });
+  }
 }
