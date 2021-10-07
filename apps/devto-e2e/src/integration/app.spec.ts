7d8ec316b7d6b056e20c52f972a1785992dbaca7
Ajit Singh <ajitsinghkaler0@gmail.com>
2021-10-07 11:29:58 +0530

    fix: change wrong names

--- apps/devto-e2e/src/integration/app.spec.ts
+++ apps/devto-e2e/src/integration/app.spec.ts
@@ -1,6 +1,6 @@
 import { getGreeting } from '../support/app.po';
 
-describe('dsh', () => {
+describe('devto', () => {
   beforeEach(() => cy.visit('/'));
 
   it('should display welcome message', () => {
@@ -8,6 +8,6 @@ describe('dsh', () => {
     // cy.login('my-email@something.com', 'myPassword');
 
     // Function helper example, see `../support/app.po.ts` file
-    getGreeting().contains('Welcome to dsh!');
+    getGreeting().contains('Welcome to devto!');
   });
 });
